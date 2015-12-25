648e13ca4413c58a57a03fe01c0243a860d63a79
李少祥 <939480781@qq.com>
2015-12-25 18:19:52 +0800

    添加阅读数

--- App/Modules/Index/Action/IndexAction.class.php
+++ App/Modules/Index/Action/IndexAction.class.php
@@ -3,26 +3,6 @@
 Class IndexAction extends Action{
     Public function index(){
     	
-       //  //读取导航
-       //  $cate=M('cate')->order('sort')->select();
-       //  import('Class.Category',APP_PATH);
-       //  $this->cate=Category::unlimitedForLayer($cate);
-        
-
-       //  //首页数据
-       // $topCate=M('cate')->where(array('pid'=>0))->order('sort')->select();
-       //  $db=M('blog');
-       //  foreach($topCate as $k => $v){
-       //  	$cids=Category::getChildsId($cate,$v['id']);
-       //  	$cids[]=$v['id'];
-        	
-       //  	$where=array('cid'=>array('IN',$cids));
-       //  	$topCate[$k]['blog']=$db->field(array('id','title','time'))->where($where)->order('time DESC')->select();
-       //  }
-
-       //  //S('index_list',$topCate,10);  //写入缓存
-       //  $this->list=$topCate;
-
         $this->display();
     }
 
@@ -31,11 +11,20 @@ Class IndexAction extends Action{
       echo json_encode($list);
     }
 
-     Public function getMovieDetail(){
+    Public function getMovieDetail(){
       $id=$_GET['movies'];
       $where=array('id'=>$id);
       $detail=M('article')->where($where)->select();
       echo json_encode($detail);
+    }
+
+    public function clickNum(){
+      
+        $id=(int)$_GET['id'];
+        M('article')->where(array('id'=>$id))->setInc('click');
+        $click= M('article')->where(array('id'=>$id))->getField('click');
+        echo $click;
+       
     }
 }
 ?>
